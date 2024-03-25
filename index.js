fc431f369f92677f71570ac6ab649694c9cfd9a9
M.A.S <160266513+m-Alishahid@users.noreply.github.com>
2024-03-25 11:44:57 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const question = await inquirer.prompt([
+    { message: 'Enter first number', type: 'number', name: 'firstNumber' },
+    { message: 'Enter second number', type: 'number', name: 'secondNumber' },
+    {
+        message: 'Please select one option to calculate',
+        type: 'list',
+        name: 'operation',
+        choices: ["Addition", "Subtraction", "Multiplication", "Division", "Other"],
+    }
+]);
+if (question.operation === "Addition") {
+    console.log(question.firstNumber + question.secondNumber);
+}
+else if (question.operation === "Subtraction") {
+    console.log(question.firstNumber - question.secondNumber);
+}
+else if (question.operation === "Multiplication") {
+    console.log(question.firstNumber * question.secondNumber);
+}
+else if (question.operation === "Division") {
+    console.log(question.firstNumber / question.secondNumber);
+}
+else {
+    console.log("This is not be acceptable!");
+}
